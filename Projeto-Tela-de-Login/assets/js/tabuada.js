fdb94ede9d2d93541111cde74c245060d94c85c6
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-02-13 18:11:00 -0300

    Utilizando o for na Tabuada

---
+++ Projeto-Tela-de-Login/assets/js/tabuada.js
@@ -0,0 +1,22 @@
+let tabuada = document.querySelector('#tabuada tbody');
+let valorA = 5;
+
+tabuada.innerHTML = '';
+
+for (let valorB = 0; valorB <= 10; valorB++) {
+
+    let resultado = valorA * valorB;
+
+    let template = `
+            <td>${valorA}</td>
+            <td>*</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>
+        `;
+    let tr = document.createElement('tr');//Cria a linha tr e insere com o comando abaixo todos os comados de 'template'.
+
+    tr.innerHTML = template;
+
+    tabuada.append(tr);
+}
