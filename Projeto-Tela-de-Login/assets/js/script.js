09adf0020c3a601ecc05ce966863ff4994418e9d
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-02-13 10:07:59 -0300

    Trabalhando com variaveis

--- Projeto-Tela-de-Login/assets/js/script.js
+++ Projeto-Tela-de-Login/assets/js/script.js
@@ -1,1 +1,7 @@
-console.log('Olá Mundo!');//Será fisível no console do navegador!
+let email = 'genilson.cavalcante105@gmail.com.br';
+
+console.log(email);//Será fisível no console do navegador!
+console.log('O seu e-mail é: '+ email);
+console.log(`O seu e-mail é: ${email}`);
+
+ //Utilize sempre 'let' para variável e 'const' para contantes
